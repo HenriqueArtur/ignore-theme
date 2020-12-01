0ef1caafff69f78ac976102e7026207d525879d9
Henrique Artur <contato@henriqueartur.com>
2020-12-01 15:32:21 -0300

    feature: render projects on front

--- templates/projects.php
+++ templates/projects.php
@@ -7,26 +7,46 @@
             </div>
         </div>
         <div class="row">
-            <div class="project col m6">
-                <div class="content">
-                    <a href="https://cosmonkeys.netlify.app/" target="_blank" rel="noopener noreferrer">
-                        <div class="title">
-                            <span>
-                                <i class="fab fa-vuejs"></i>
-                            </span>
-                            <span> Site CosMonkes</span>
+            <?php
+            $query_projects = new WP_Query( array(
+                'post_type' => 'projects',
+                'order'   => 'ASC',
+            ));
+
+            if( $query_projects->have_posts() ) {
+                while( $query_projects->have_posts() ) {
+                    $query_projects->the_post();
+                    ?>
+                    <div class="project col m6">
+                        <div class="content">
+                            <a href="<?php echo get_post_meta( get_the_ID(), 'link_project_key', true ) ?>" target="_blank" rel="noopener noreferrer">
+                                <div class="title">
+                                    <span>
+                                        <i class="fab fa-<?php echo get_post_meta( get_the_ID(), 'icon_key', true ) ?>"></i>
+                                    </span>
+                                    <span> <?php echo the_title(); ?></span>
+                                </div>
+                                <div class="description"><?php echo get_post_meta( get_the_ID(), 'description_key', true ) ?></div>
+                                <?php $tags = get_post_meta( get_the_ID(), 'tags_key', true ) ?>
+                                <div class="tags">
+                                    <?php
+                                    if(count($tags) >= 1) {
+                                        foreach ($tags as $tag) {
+                                            ?>
+                                            <span class="tag"><?php echo $tag; ?></span>
+                                            <?php
+                                        }
+                                    }
+                                    ?>
+                                </div>
+                            </a>
                         </div>
-                        <div class="description">
-                            Desenvolvimento de site em Vue.js, Nuxt e Buefy para o estudio de desenvolvimento de jogos.
-                        </div>
-                        <div class="tags">
-                            <span class="tag">Vue.js</span>
-                            <span class="tag">Nuxt</span>
-                            <span class="tag">Buefy</span>
-                        </div>
-                    </a>
-                </div>
-            </div>
+                    </div>
+                    <?php
+                }
+            }
+            wp_reset_query();
+            ?>
         </div>
     </div>
 </section>
